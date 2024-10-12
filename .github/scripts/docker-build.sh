d92f5f266d4c6648907f3d4deced458f3ee8bdaa
Cocoa <i@uwucocoa.moe>
2024-10-12 21:23:59 +0100

    fix ci
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -11,4 +11,4 @@ TARGET=$6
 JOBS=$7
 
 sudo docker run --privileged --network=host --rm --platform="${PLATFORM}" -v $(pwd):/work "${IMAGE}" \
-    sh -c "chmod a+x /work/install-deps-linux.sh && ./install-deps-linux.sh ${TARGET} ${IMAGE} && chmod a+x /work/do-build.sh && /work/do-build.sh /work ${BUILD_VERSION_V} ${LIBRARY_TYPE} ${TARGET} ${JOBS}"
+    sh -c "cd /work && chmod a+x ./install-deps-linux.sh && ./install-deps-linux.sh ${TARGET} ${IMAGE} && chmod a+x ./do-build.sh && ./do-build.sh /work ${BUILD_VERSION_V} ${LIBRARY_TYPE} ${TARGET} ${JOBS}"
