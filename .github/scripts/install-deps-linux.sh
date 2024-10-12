e0bb9391249e75375ad0e5209b24fd92ff05ba22
Cocoa <i@uwucocoa.moe>
2024-10-12 23:04:40 +0100

    22.04
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/scripts/install-deps-linux.sh
+++ .github/scripts/install-deps-linux.sh
@@ -19,6 +19,11 @@ case $RUNS_ON in
     ${SUDO} apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev liblzma-dev libcurl4-openssl-dev libnotify-dev \
       libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev gcc g++ make
     ;;
+  ubuntu*22.04)
+    ${SUDO} apt-get update
+    ${SUDO} apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev liblzma-dev libcurl4-openssl-dev libnotify-dev \
+      libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev gcc g++ make
+    ;;
   ubuntu*24.04)
     ${SUDO} apt-get update
     ${SUDO} apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev liblzma-dev libcurl4-openssl-dev libnotify-dev \
