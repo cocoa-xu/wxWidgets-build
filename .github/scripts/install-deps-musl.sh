4451c9b4990111b163c50746302b81e37cb05cf0
Cocoa <i@uwucocoa.moe>
2024-10-13 20:14:25 +0100

    updated ci
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/scripts/install-deps-musl.sh
+++ .github/scripts/install-deps-musl.sh
@@ -14,7 +14,7 @@ else
 fi
 
 ${SUDO} apk update
-${SUDO} apk add gcc g++ curl curl-dev make curl git gtk+3.0-dev glu-dev bash perl-utils
+${SUDO} apk add gcc g++ curl curl-dev make curl git gtk+3.0-dev glu-dev bash perl-utils gspell-dev libsecret-dev mesa-dev mesa-gl
 
 git clone --branch "v${BUILD_VERSION}" https://github.com/wxWidgets/wxWidgets.git "${ROOTDIR}/wxWidgets"
 cd "${ROOTDIR}/wxWidgets"
