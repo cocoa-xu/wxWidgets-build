a72b1387491039c2cc2e8a36e3fd2c3f17fd7d03
Cocoa <i@uwucocoa.moe>
2024-10-13 19:11:11 +0100

    updated ci
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -10,5 +10,5 @@ LIBRARY_TYPE=$5
 TARGET=$6
 JOBS=$7
 
-sudo docker run --privileged --network=host --rm --platform="${PLATFORM}" -v $(pwd):/work "${IMAGE}" \
-    sh -c "cd /work && chmod a+x ./install-deps.sh && ./install-deps.sh ${TARGET} ${IMAGE} && chmod a+x ./do-build.sh && ./do-build.sh /work ${BUILD_VERSION} ${LIBRARY_TYPE} ${TARGET} ${JOBS}"
+sudo docker run --privileged --network=host --rm --platform="${PLATFORM}" -v "$(pwd)":/work "${IMAGE}" \
+    sh -c "chmod a+x /work/install-deps.sh && /work/install-deps.sh ${TARGET} ${IMAGE} && chmod a+x /work/do-build.sh && /work/do-build.sh /work ${BUILD_VERSION} ${LIBRARY_TYPE} ${TARGET} ${JOBS}"
