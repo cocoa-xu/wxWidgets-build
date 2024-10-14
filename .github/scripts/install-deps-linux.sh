f89736b888886cf0f1a72ee11698ecc7152b9290
Cocoa <i@uwucocoa.moe>
2024-10-14 23:15:10 +0100

    armv7l
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/scripts/install-deps-linux.sh
+++ .github/scripts/install-deps-linux.sh
@@ -17,17 +17,17 @@ case $RUNS_ON in
   ubuntu*20.04)
     ${SUDO} apt-get update
     ${SUDO} apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev liblzma-dev libcurl4-openssl-dev libnotify-dev \
-      libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev gcc g++ make
+      libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev gcc g++ make git
     ;;
   ubuntu*22.04)
     ${SUDO} apt-get update
     ${SUDO} apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev liblzma-dev libcurl4-openssl-dev libnotify-dev \
-      libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev gcc g++ make
+      libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev gcc g++ make git
     ;;
   ubuntu*24.04)
     ${SUDO} apt-get update
     ${SUDO} apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev liblzma-dev libcurl4-openssl-dev libnotify-dev \
-      libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev gcc g++ make
+      libsecret-1-dev libgspell-1-dev libgl1-mesa-dev libglu1-mesa-dev gcc g++ make git
     ;;
   *)
     echo "Unknown platform: $RUNS_ON"
